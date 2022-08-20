7f06551016e08cd7c6ea265a3aecb8d5a5e62ecf
Damir <damirmelis@gmail.com>
2022-08-20 18:28:00 +0600

    Some update

--- itfinder/urls.py
+++ itfinder/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', include('users.urls')),
 ]
 
-"""маршруты к папкам статик и медиа"""
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
